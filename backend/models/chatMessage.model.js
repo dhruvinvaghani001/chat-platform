4cd9b51a216282d7aea8ca81777413f0d80974cb
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-02-27 10:22:04 +0530

    chat message model

--- backend/models/chatMessage.model.js
+++ backend/models/chatMessage.model.js
@@ -1,8 +1,25 @@
 import mongoose from "mongoose";
 
-const messageShcema = new mongoose.Schema({}, { timestamps: true });
+const messageShcema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    chat:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Chat"
+    }
+    
+  },
+  { timestamps: true }
+);
 
 const Chatmessage = mongoose.model("Chatmessage", messageShcema);
 
 export default Chatmessage;
- 
