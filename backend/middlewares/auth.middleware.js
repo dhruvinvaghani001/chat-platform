c0ff2e36c8a4d72ebff1a60224aee922a68b7733
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-03-04 22:51:53 +0530

    edit : veifyJWT as requirement for frontend

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -5,21 +5,29 @@ import jwt from "jsonwebtoken";
 /**
  * middleware to provide logged in user details to all protected routes
  */
-const VerifyJWT = asyncHandler(async (req, res, next) => {
-  const token =
-    req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "");
+const VerifyJWT = async (req, res, next) => {
+  try {
+    const token =
+      req.cookies?.token || req.header("Authorization")?.replace("Bearer ", "");
 
-  if (!token) {
-    return res.status(401).json(new ApiError(401, "Unauthenticated user !"));
+    if (!token) {
+      return res.status(401).json(new ApiError(401, "Unauthenticated user !"));
+    }
+
+    const decodedData = await jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    if (!decodedData) {
+      return res.status(401).json(new ApiError(401, "token expirres"));
+    } else {
+      req.user = decodedData;
+      next();
+    }
+  } catch (error) {
+    return res.status(401).json(new ApiError(401, "token expires!"));
   }
-
-  const decodedData = await jwt.verify(token, process.env.JWT_SECRET_KEY);
-  if (!decodedData) {
-    return res.status(401).json(new ApiError(401, "token expirres"));
-  }
-
-  req.user = decodedData;
-  next();
-});
+};
 
 export default VerifyJWT;
