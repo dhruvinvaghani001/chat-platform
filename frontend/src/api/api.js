b55a828dde72f30fe0ab1d974162587514172374
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-03-05 19:58:24 +0530

    added: api function

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -28,7 +28,7 @@ axiosInstance.interceptors.response.use(
     return response;
   },
   function (error) {
-    // error.message = error?.response?.data?.message;
+    error.message = error?.response?.data?.message;
     return Promise.reject(error);
   }
 );
@@ -57,4 +57,38 @@ const searchAvailableUser = () => {
   return axiosInstance.get("/chat/available-user");
 };
 
-export { autheticateUser, logout, searchAvailableUser };
+/**
+   * Creates a one-to-one chat with the specified user.
+   * @param {ObjectId} userId - The ID of the user to create the chat with.
+     @returns {Promise} - A promise that resolves to the response of the API call.
+*/
+const createOneToOneChat = (userId) => {
+  return axiosInstance.post(`/chat/c/${userId}`);
+};
+
+/**
+ * get all chats in which user is part of it ;
+ * @returns a promise that resolve to the response of the api call
+ */
+
+const getAllChats = () => {
+  return axiosInstance.get("/chat");
+};
+
+/**
+ * Create group chat
+ * @params : data ({name , members})
+ * @returns a promise that resolve the response of api call
+ */
+const createGroup = (data) => {
+  return axiosInstance.post("/chat/group", data);
+};
+
+export {
+  autheticateUser,
+  logout,
+  searchAvailableUser,
+  createOneToOneChat,
+  getAllChats,
+  createGroup
+};
