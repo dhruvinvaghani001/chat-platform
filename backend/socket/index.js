bffce3778effa924c3734d7b6501d85e2528212b
dhruvinvaghani0011 <dhruvinvaghani0011@gmail.com>
2024-03-06 16:44:27 +0530

    added : socket server

---
+++ backend/socket/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("user joined socket", socket.id);
+
+  socket.on("setup", ({ userId }) => {
+    console.log("user joined room :" + userId);
+    socket.join(userId.toString());
+  });
+
+  socket.on("new-message", ({ message, chat }) => {
+    console.log(message);
+    console.log(chat);
+    const members = chat.members.filter(
+      (memebr) => memebr._id != message.sender._id
+    );
+    console.log(members) 
+    members.forEach((element) => {
+      socket.in(element._id.toString()).emit("new message recived", message);
+    });
+  });
+});
+
+export { app, server, io };
